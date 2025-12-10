201db4af43c80a29a7cc919216b725c2f8b743f5
Ryan-F3107 <ryanfernandes3107@gmail.com>
2025-12-10 11:00:42 +0530

    added small banner

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -5,6 +5,7 @@ import { cn } from "../lib/utils";
 export const ThemeToggle = () => {
     /* store & update value of dark/light mode */
     const [isDarkMode, setIsDarkMode] = useState(false);
+    const [showBanner, setShowBanner] = useState(false);
     /* useEffect
     Initializes the theme based on saved preferences
     empty dependency = runs once
@@ -17,6 +18,14 @@ export const ThemeToggle = () => {
         } else {
             localStorage.setItem("theme", "light"); //sets localStorage object with key/value pair-light
             setIsDarkMode(false);
+        }
+
+        //Banner Logic:
+        //Shows if user is in light mode and has not seen the banner
+        const hasSeenBanner = localStorage.getItem("seenDarkModeNotice");
+        if (!hasSeenBanner && storedTheme !=="dark"){
+            setShowBanner(true);
+            localStorage.setItem("seenDarkModeNotice","true");
         }
     }, [])
     /* triggers when theme toggle button is clicked */
@@ -34,6 +43,7 @@ export const ThemeToggle = () => {
         }
     }
     return (
+        <>
         <button onClick={toggleTheme} className={cn(
             "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
             "focus:outlin-hidden"
@@ -44,5 +54,18 @@ export const ThemeToggle = () => {
                 <Moon className="h-6 w-6 text-blue-900"/>
              )}
         </button>
-            );
+        {/* Banner & toast */}
+        {showBanner && (
+            <div className="fixed bottom-4 left-1/2 -translate-x-1/2 px-4 py-2 bg-gray-900 text-white rounded-md shadow-lg text-sm flex items-center gap-3 z-50">
+          <span>🌙 This site looks best in dark mode.</span>
+          <button
+            onClick={() => setShowBanner(false)}
+            className="bg-primary/80 hover:bg-primary px-3 py-1 rounded-md text-xs font-medium"
+          >
+            Got it
+          </button>
+        </div>
+        )}
+        </>
+    );
 }
